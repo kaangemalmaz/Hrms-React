6691efa25cc4b5ff7b2546927e3263bee6863c5f
DESKTOP-46BN8NO\Kaan <n0syste3m65@hotmail.com>
2022-01-02 18:01:27 +0300

    cityandworktypetime_filter

--- src/pages/JobPostings/JobPostingsList.jsx
+++ src/pages/JobPostings/JobPostingsList.jsx
@@ -1,28 +1,51 @@
+import { Formik, Form } from 'formik';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { Button, Icon, Menu, Table } from 'semantic-ui-react';
+import { Button, Grid, Icon, Menu, Table } from 'semantic-ui-react';
+import CityService from '../../services/cityService';
 import FavoriteJobPosting from '../../services/favoriteJobPosting';
 import JobPostingService from '../../services/jobPostingService';
+import TypeOfWorkTimeService from '../../services/typeOfWorkTimeService';
+import AKGDropdown2 from '../../utilities/CustomFormControl/AKGDropdown2';
 
 
 export default function JobPostingsList() {
+    
+    //job posting için favorilere ekleme için candidate id yi almayı sağlar.
     let { id } = useParams();
-    let favoriteJobPosting = new FavoriteJobPosting();
+
     const [jobPostings, setJobPostings] = useState([]);
+    const [cities, setCities] = useState([]);
+    const [typeOfWorkTimes, setTypeOfWorkTime] = useState([]);
+
+    let jobPostingService = new JobPostingService();
+    let favoriteJobPosting = new FavoriteJobPosting();
+    let cityService = new CityService();
+    let typeOfWorkTimeService = new TypeOfWorkTimeService();
 
     useEffect(() => {
         //component yüklendiğinde yapılmasını istediğin şeyi buraya yazıyorsun bu demek oluyorki sayfa yüklendiğinde aslında bu metod çalışacak.
-        let jobPostingService = new JobPostingService();
-        jobPostingService.getAll().then(result => setJobPostings(result.data.data))
-        //burada result.data
-        //         "success": true,
-        //   "message": "Ürünler Listelendi.",
-        //   "data": [
-        //hepsini birden döndürür biz buradan sadece datayı alacağımız için data.data şeklinde yazdık unutma!
-    })
+        jobPostingService.getAll().then(result => setJobPostings(result.data.data));
+        cityService.getAll().then((result) => setCities(result.data.data));
+        typeOfWorkTimeService.getAll().then((result) => setTypeOfWorkTime(result.data.data));
+    }, [])
 
+    //filter için iş çalışma zamanı tip bilgilerini getirir
+    const typeOfWorkTimeOptions = typeOfWorkTimes.map((typeOfWorkTime, index) => ({
+        key: index,
+        text: typeOfWorkTime.workTimeType,
+        value: typeOfWorkTime.id
+    }))
+
+    //filter için şehir bilgilerini getirir
+    const cityOptions = cities.map((city, index) => ({
+        key: index,
+        text: city.name,
+        value: city.id
+    }))
+
+    //favori iş ilanlarını eklemeyi sağlar.
     const onSubmit = (values) => {
         let favoriteJobDto = {
             candidate: { id: id },
@@ -32,10 +55,51 @@ export default function JobPostingsList() {
         toast.success(`${values.job.title} başarı ile favorilere eklenmiştir.`)
     }
 
+
+    //hatalı atama olmaması için filter için 0 ataması yapar.
+    const initialValue = {
+        cityId: 0,
+        typeOfWorkTimeId: 0
+    }
+
+    //Filter için hangi servisi çağıracağını belirler burada 0 olayını initialvalue de alır.
+    const onFilter = (values) => {
+        if (values.cityId !== 0 && values.typeOfWorkTimeId === 0) {
+            jobPostingService.getBycityId(values.cityId).then(result => setJobPostings(result.data.data));
+            toast.success("Filtreleme işlemi yapılmıştır.");
+        }
+        else if (values.typeOfWorkTimeId !== 0 && values.cityId === 0) {
+            jobPostingService.getBytypeOfWorkTimeId(values.typeOfWorkTimeId).then(result => setJobPostings(result.data.data));
+            toast.success("Filtreleme işlemi yapılmıştır.");
+        }
+        else if (values.typeOfWorkTimeId !== 0 && values.cityId !== 0) {
+            jobPostingService.getBycityIdAndtypeOfWorkTimeId(values.cityId, values.typeOfWorkTimeId).then(result => setJobPostings(result.data.data));
+            toast.success("Filtreleme işlemi yapılmıştır.");
+        }
+        else {
+            //jobPostingService.getAll().then(result => setJobPostings(result.data.data));
+            toast.info("Filtreleme için seçim yapmalısınız!!");
+        }
+    }
+
+
+    //direk ekranı yeniler. //temizle arkasında çalışır.
+    const handleClick = () => window.location.reload()
+
     return (
         <div>
             {/* <Button basic color='yellow'><Link to={"/jobpostings/add"}>İş ekleyiniz.</Link></Button> */}
-            
+            <Formik initialValues={initialValue} onSubmit={onFilter} >
+                <Form className="ui form" >
+                    <Grid columns='equal'>
+                        <Grid.Column width={6}><AKGDropdown2 name="cityId" defaultOption="Şehir Seçiniz" options={cityOptions} /></Grid.Column>
+                        <Grid.Column width={6}><AKGDropdown2 name="typeOfWorkTimeId" defaultOption="Çalışma Zaman Tipi Seçiniz" options={typeOfWorkTimeOptions} /></Grid.Column>
+                        <Grid.Column width={2}><Button color="green" type="submit" >Filtre</Button></Grid.Column>
+                        <Grid.Column width={2}><Button color='grey' type="submit" onClick={()=> handleClick()}>Temizle</Button></Grid.Column>
+                    </Grid>
+                </Form>
+            </Formik>
+
             <Table celled>
                 <Table.Header>
                     <Table.Row>
@@ -96,6 +160,6 @@ export default function JobPostingsList() {
                 </Table.Footer>
 
             </Table>
-        </div>
+        </div >
     )
 }
--- src/services/jobPostingService.js
+++ src/services/jobPostingService.js
@@ -1,23 +1,49 @@
 import axios from "axios";
 
-export default class JobPostingService{
-    getAll(){
-        return axios.get("http://localhost:8080/api/jobpostings/findByIsActiveTrue");
-    }
+export default class JobPostingService {
+  getAll() {
+    return axios.get(
+      "http://localhost:8080/api/jobpostings/findByIsActiveTrue"
+    );
+  }
 
-    getById(id){
-        return axios.get("http://localhost:8080/api/jobpostings/getById?id=" + id);
-    }
+  getById(id) {
+    return axios.get("http://localhost:8080/api/jobpostings/getById?id=" + id);
+  }
 
-    getAllActiveFalse(){
-        return axios.get("http://localhost:8080/api/jobpostings/findByIsActiveFalse");
-    }
+  getAllActiveFalse() {
+    return axios.get(
+      "http://localhost:8080/api/jobpostings/findByIsActiveFalse"
+    );
+  }
 
-    getAllActiveTrue(){
-        return axios.get("http://localhost:8080/api/jobpostings/findByIsActiveTrue");
-    }
+  getAllActiveTrue() {
+    return axios.get(
+      "http://localhost:8080/api/jobpostings/findByIsActiveTrue"
+    );
+  }
 
-    add(values){
-        return axios.post("http://localhost:8080/api/jobpostings/add", values);
-    }
+  add(values) {
+    return axios.post("http://localhost:8080/api/jobpostings/add", values);
+  }
+
+  getBycityId(cityId) {
+    return axios.get(
+      "http://localhost:8080/api/jobpostings/getBycityId?cityId=" + cityId
+    );
+  }
+
+  getBytypeOfWorkTimeId(typeOfWorkTimeId) {
+    return axios.get(
+      "http://localhost:8080/api/jobpostings/getBytypeOfWorkTimeId?typeOfWorkTimeId=" +
+        typeOfWorkTimeId
+    );
+  }
+
+  getBycityIdAndtypeOfWorkTimeId(cityId, typeOfWorkTimeId) {
+    return axios.get(
+      "http://localhost:8080/api/jobpostings/getBycityIdAndtypeOfWorkTimeId?cityId=" +cityId +
+        "&typeOfWorkTimeId=" +typeOfWorkTimeId
+    );
+  }
 }
